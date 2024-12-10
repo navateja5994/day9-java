c6d7affbb24c49b14355686e8fb27ddc0f01e710
NAVATEJA <navatejar80@gmail.com>
2024-12-10 09:51:47 +0530

    Add files via upload

---
+++ string2.java
@@ -0,0 +1,9 @@
+class main{
+    public static void main(String args[])
+    {
+        String a="Hello";
+        String b=new String("Tej");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
