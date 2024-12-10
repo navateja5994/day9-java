1755d520a55eb70f6fbb98a5808dd2ffec8be323
NAVATEJA <navatejar80@gmail.com>
2024-12-10 10:56:01 +0530

    Add files via upload

---
+++ next.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class main{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println(a);
+        String b=sc.nextLine();
+        System.out.println(b);
+    }
+}
