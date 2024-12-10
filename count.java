a15a5fe0a02991e51055af27b3a1f888bf1ac414
NAVATEJA <navatejar80@gmail.com>
2024-12-10 12:06:53 +0530

    Add files via upload

--- count.java
+++ count.java
@@ -1,32 +1,40 @@
 import java.util.Scanner;
-
-class CharacterCount {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-        
-        int vowels = 0, consonants = 0, numbers = 0, specials = 0;
-        
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            
-            if (Character.isDigit(ch)) {
-                numbers++;
-            } else if (Character.isLetter(ch)) {
-                if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                    vowels++;
-                } else {
-                    consonants++;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        int vowel = 0;
+        int consonant = 0;
+        int number = 0;
+        int special = 0;
+        for (int i = 0; i < a.length(); i++)
+        {
+            char c=a.charAt(i);
+            if (Character.isDigit(c))
+            {
+                number++;
+            }
+            else if (Character.isLetter(c))
+            {
+                if (c=='A'||c=='E'||c=='I'||c=='O'||c=='U'||c=='a'||c=='e'||c=='i'||c=='o'||c=='u') {
+                    vowel++;
                 }
-            } else {
-                specials++;
+                else
+                {
+                    consonant++;
+                }
+            } 
+            else 
+            {
+                special++;
             }
         }
         
-        System.out.println("Vowels: " + vowels);
-        System.out.println("Consonants: " + consonants);
-        System.out.println("Numbers: " + numbers);
-        System.out.println("Special Characters: " + specials);
+        System.out.println("Vowels: " + vowel);
+        System.out.println("Consonants: " + consonant);
+        System.out.println("Numbers: " + number);
+        System.out.println("Special Characters: " + special);
     }
 }
