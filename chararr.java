2ac8e61605db2104fd40bd2531b2d70523326b35
NAVATEJA <navatejar80@gmail.com>
2024-12-10 14:38:49 +0530

    Add files via upload

---
+++ chararr.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        String a="AMARAN";
+        int c[]=new int[26];
+        for(int i=0;i<a.length();i++)
+        {
+            c[((int)(a.charAt(i)))-65]++;
+        }
+        System.out.println(Arrays.toString(c));
+    }
+}
